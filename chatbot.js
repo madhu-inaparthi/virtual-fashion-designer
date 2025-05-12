1ed7cd51c2e8dda4f42e6f60d42605953d24dd29
madhu-software <inaparthi.madhukiran.s89@kalvium.community>
2025-05-12 14:39:19 +0530

    netlify test

--- chatbot.js
+++ chatbot.js
@@ -78,7 +78,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173','https://tangerine-scone-7cf83d.netlify.app/');
+    res.header('Access-Control-Allow-Origin', 'https://tangerine-scone-7cf83d.netlify.app/');
     res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
